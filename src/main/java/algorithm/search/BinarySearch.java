ddea7eac1ab4c33362567089d2ee297e52e75528
Kunlei Lian <kunlei1987@gmail.com>
2022-01-08 16:43:33 -0600

    update

--- src/main/java/algorithm/search/BinarySearch.java
+++ src/main/java/algorithm/search/BinarySearch.java
@@ -2,31 +2,46 @@ package algorithm.search;
 
 public class BinarySearch {
 
-  public static <T extends Comparable<T>> int find(T[] sortedArray, T target) {
-    return search(sortedArray, 0, sortedArray.length - 1, target);
+  /**
+   * find the index of the target in the sorted list
+   * @param sortedList input list of sorted elements
+   * @param target target element
+   * @param <T> element type
+   * @return index of the target element, -1 if not found
+   */
+  public static <T extends Comparable<T>> int find(T[] sortedList, T target) {
+    return search(sortedList, 0, sortedList.length - 1, target);
   }
 
-  public static <T extends Comparable<T>> int search(T[] sortedArray, int startingIdx, int endingIdx, T target) {
+  /**
+   * find the index of the target element from the given range of a sorted list
+   * @param sortedList sorted list
+   * @param startingIdx starting index, inclusive
+   * @param endingIdx ending index, inclusive
+   * @param target target element
+   * @param <T> element type
+   * @return index of the target, -1 if not found
+   */
+  public static <T extends Comparable<T>> int search(T[] sortedList, int startingIdx, int endingIdx, T target) {
     if (endingIdx < startingIdx) {
       return -1;
     }
 
     int midIdx = startingIdx + (endingIdx - startingIdx) / 2;
-    int comparison = target.compareTo(sortedArray[midIdx]);
+    int comparison = target.compareTo(sortedList[midIdx]);
     if (comparison == 0) {
       return midIdx;
     } else if (comparison < 0) {
-      return search(sortedArray, 0, midIdx -1, target);
+      return search(sortedList, 0, midIdx -1, target);
     } else {
-      return search(sortedArray, midIdx + 1, endingIdx, target);
+      return search(sortedList, midIdx + 1, endingIdx, target);
     }
   }
 
   public static void main(String[] args) {
-    Integer[] array = new Integer[]{1, 3, 5, 9, 10, 15, 21, 100, 100};
+    Integer[] array = new Integer[]{1, 3, 5, 9, 10, 15, 21, 100, 200};
 
-    int target = 21;
-
+    int target = 100;
     int idx = find(array, target);
     System.out.println("idx = " + idx);
   }
